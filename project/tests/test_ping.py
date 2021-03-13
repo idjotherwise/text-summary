9cc9f6ebed3d6777d2b5dba1dbca6a6cb385e02f
Ifanj <ifan.johnston@gmail.com>
2021-03-13 20:31:59 +0100

    Fix deploy workflow and changed 'pong!' to 'pong' to test out the workflow

--- project/tests/test_ping.py
+++ project/tests/test_ping.py
@@ -6,4 +6,4 @@ def test_ping(test_app):
 
     # Then: (asserts)
     assert response.status_code == 200
-    assert response.json() == {"environment": "dev", "ping": "pong!", "testing": True}
+    assert response.json() == {"environment": "dev", "ping": "pong", "testing": True}
