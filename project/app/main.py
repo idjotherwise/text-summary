1d360563bd8cec1934d542550915ebab5139ec68
Ifanj <ifan.johnston@gmail.com>
2021-03-14 21:31:59 +0100

    Add a home view that shows all the links and summaries from the database.

--- project/app/main.py
+++ project/app/main.py
@@ -1,19 +1,23 @@
 import logging
 
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
 
 from app.api import ping, summaries
 from app.db import init_db
+from app.views import home
 
 log = logging.getLogger("uvicorn")
 
 
 def create_application() -> FastAPI:
     application = FastAPI()
+    application.mount('/app/static', StaticFiles(directory='app/static'), name='static')
     application.include_router(ping.router)
     application.include_router(
         summaries.router, prefix="/summaries", tags=["summaries"]
     )
+    application.include_router(home.router)
 
     return application
 
